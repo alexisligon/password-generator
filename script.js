4f9bc35f6920d5e5b9d05e77694dae9bd130d861
Alexis Ligon <aligon17@gmail.com>
2021-04-16 10:31:28 -0400

    cleaned up code, added more comments, deleted console.logs

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
+//variables of characters to select from
 var charSelected = "";
-
-  var special = " !@#$%^&*+',-./:;<=>?~\|";
-  var upper= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var lower= "abcdefghijklmnopqrstuvwxyz";
-  var num= "0123456789";
+var special = " !@#$%^&*+',-./:;<=>?~\|";
+var upper= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lower= "abcdefghijklmnopqrstuvwxyz";
+var num= "0123456789";
 
 
 
@@ -15,9 +15,7 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 
@@ -28,66 +26,48 @@ function generatePassword() {
 var ask1 = window.confirm("Should your password contain uppercase letters?");
 if (ask1 === true) {
   charSelected = charSelected + upper;//pull from upper string
-  console.log (charSelected);
 } else {
   charSelected = charSelected;
-  console.log (charSelected);
 }
 
 //ask for lowercase
 var ask2 = window.confirm ("Should your password contain lowercase letters?");
 if (ask2 === true) {
   charSelected = charSelected + lower;//pull from lower string
-  console.log (charSelected);
 } else {
   charSelected = charSelected;
-  console.log (charSelected);
 }
 
 //ask for numbers
 var ask3 = window.confirm ("Should your password contain numbers?");
 if (ask3 === true) {
   charSelected = charSelected + num;//pull from numb string
-  console.log (charSelected);
 } else {
   charSelected = charSelected;
-  console.log (charSelected);
 }
 
 //ask for special characters
 var ask4 = window.confirm ("Should your password contain special characters?");
 if (ask4 === true) { 
-  charSelected = charSelected + special;//pull from char string
-  console.log (charSelected);
+  charSelected = charSelected + special;//pull from char string 
 } else {
-  charSelected = charSelected;
-  console.log (charSelected);
+  charSelected = charSelected; 
 }
 
 //ask for password length
 var passnum = parseInt(window.prompt("How many characters should your password be? Choose a number between 8 and 128")); //parseInt allows input to be interger and not a string
-if (passnum >= 8 && passnum <=128) 
-{ //allows only input between 8 and 128
- console.log ('passnum', passnum);
-
- var theFinalPassword = '' //starting blank password to add to
+if (passnum >= 8 && passnum <=128){ //allows only input between 8 and 128
+ var theFinalPassword = '' //starting blank password to add random characters to
  for (var i = 0; i < passnum; i++) { //runs the loop for the selected number of times
-   console.log('index ', i)
-  console.log('char selected ', charSelected.length)
-
   var passwordText = charSelected.charAt(Math.floor(Math.random() * charSelected.length));//charAt pulls from the entire string, Math.floor Math.random makes the selection random
-  
   theFinalPassword = theFinalPassword.concat(passwordText) // adds each random selection to the blank starter
-
    }
-   console.log('THE FINAL PASSWORD!!!!! ', theFinalPassword);
-   return theFinalPassword;
+   return theFinalPassword; //displays the final randomly selected password
 }
  else {
   alert("Please choose between 8 and 128")
-  return; // need to stop loop if input outside of parameter
+  return; //stops loop if input outside of parameter
 }
-
 
 }
 
